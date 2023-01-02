36885d07824ecad2cf51e5338e1c2395c0729a5a
Fabian Weik <fabian.weik@protonmail.com>
2023-01-02 14:58:46 +0100

    parse current configuration of cobweb

--- Simulation/move-cobweb.sh
+++ Simulation/move-cobweb.sh
@@ -42,8 +42,50 @@ cd "$MODEL_DIR"
 DIAGRAM_DIR="${MODEL_DIR}/${DIAGRAM_NAME}"
 [ ! -d "$DIAGRAM_DIR" ] && echo "No dir ${DIAGRAM_NAME} in ${MODEL_DIR}" && exit 1
 
+# Load current parameters
+
+declare -A PARAMETER_VALUES
+declare -A PARAMETER_LINES
+
+CONFIG_FILE="${DIAGRAM_DIR}/config.ant"
+LINE_NUM="0"
+PARSING=""
+while read -r LINE; do
+    LINE_NUM=$((LINE_NUM + 1))
+
+    if [ -z "${PARSING}" ]; then
+        [[ "${LINE}" =~ ^parameter"[".+"]" ]] && PARSING="true" 
+    else
+        if [[ "${LINE}" =~ ^\},?$ ]]; then
+            PARSING=""
+
+            PARAMETER_VALUES["${NAME}"]="${VALUE}"
+            PARAMETER_LINES["${NAME}"]="${VALUE_LINE}"
+        else
+            _NAME="$(echo "${LINE}" | grep ^name | sed -r 's/name\s*=\s*"([^"]*)"/\1/')"
+            [ -n "$_NAME" ] && NAME="${_NAME}"
+            _VALUE="$(echo "${LINE}" | grep ^value | sed -r 's/value\s*=\s*([^,]*)[,$]/\1/')"
+            [ -n "$_VALUE" ] && VALUE="${_VALUE}" && VALUE_LINE="${LINE_NUM}"
+        fi
+    fi
+done < "${CONFIG_FILE}"
+
+echo "Parsed ${CONFIG_FILE}..."
+echo
+for NAME in "${!PARAMETER_VALUES[@]}"; do
+    echo -e "${NAME}: \t${PARAMETER_VALUES["${NAME}"]} \t in line ${PARAMETER_LINES["${NAME}"]}"
+done
+echo
+
+
 if [ -n "$COPY_NAME" ]; then
+    
+    # TODO: output what's happening
+
     DIAGRAM_COPY_DIR="${MODEL_DIR}/${COPY_NAME}"
     cp -r "${DIAGRAM_DIR}" "${DIAGRAM_COPY_DIR}"
     DIAGRAM_DIR="${DIAGRAM_COPY_DIR}"
+    CONFIG_FILE="${DIAGRAM_DIR}/config.ant"
 fi
+
+# TODO: adjust parameters
