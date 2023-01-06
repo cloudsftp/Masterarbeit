24009aba4e07549705e31dfc4d380fb46619b81b
Fabian Weik <fabian.weik@protonmail.com>
2023-01-06 12:27:17 +0100

    add original data by yunus

---
+++ Simulation/Models/99_Yunus/Original/Pointsinchain/2/4/invertorNew.cpp
@@ -0,0 +1,169 @@
+#include "AnT.hpp"
+
+#define mod(a,b) ((a)-(floor((a)/(b))*(b)))
+
+#define E0 parameters[0]
+#define hi parameters[1]
+#define mu parameters[2]
+
+#define R (2)
+#define L 4.2E-3
+#define Vref 5.0
+#define bt 1
+
+#define Lr1 (-R/L)
+#define T (1.0/150)
+#define q (R*Vref/bt/E0)
+#define omega (2*Pi/T)
+#define Lr (Lr1/omega)
+
+
+#define chi (R*hi/bt/E0)
+
+real_t F(real_t xk, real_t z, real_t Kp, int StepAlg, const Array<real_t>& parameters)
+{
+  if (StepAlg == 1)
+  {
+    return q*cos(xk+z)-Kp*chi-(q*cos(xk)-Kp*mu*chi)*exp(Lr*z);
+  }
+  else if (StepAlg == 2)
+  {
+    return q*cos(xk+z)+Kp*chi-(q*cos(xk)-Kp*mu*chi)*exp(Lr*z);
+  }
+  else
+  {
+    return Kp+(q*cos(xk)-Kp*chi-Kp)*exp(Lr*z)-q*cos(xk+z)+Kp*chi*mu;
+  }
+}
+
+real_t half(real_t xk, real_t Kp, int StepAlg, const Array<real_t>& parameters)
+{
+  real_t zk, za, zb, z, fk, fa, fb;
+  zk = 0;
+  fa = F(xk, zk, Kp, StepAlg, parameters);
+  do
+  {
+    za = zk;
+    fa = F(xk, za, Kp, StepAlg, parameters);
+    zk += 0.02;
+    fk = F(xk, zk, Kp, StepAlg, parameters);
+  } while (fa*fk > 0);
+
+  zb = zk;
+  fa = F(xk, za, Kp, StepAlg, parameters);
+  fb = F(xk, zb, Kp, StepAlg, parameters);
+
+  do
+  {
+    z = 0.5*(za+zb);
+
+    fk = F(xk, z, Kp, StepAlg, parameters);;
+    if (fa*fk < 0) zb = z;
+    else
+    {
+      za = z;
+      fa = fk;
+    }
+  } while (fabs(za-zb)>1E-15);
+
+  z = 0.5*(za+zb);
+
+  return z;
+}
+
+
+bool invertor (const Array<real_t>& currentState,
+  const Array<real_t>& parameters,
+	Array<real_t>& RHS)
+{
+  real_t xk, Kp;
+  real_t et;
+  real_t tk, tk1, tk2;
+  real_t z, z_0, z_k;
+  int StepAlg;
+
+  xk = currentState[0];
+
+  static long K = 0;
+  static bool finish = false;
+
+  if ((xk>=0) and (xk<Pi/2))
+  {
+    Kp = 1;
+  }
+  else
+  {
+    if ((xk>=3.0/2*Pi) and (xk<=2*Pi))
+    {
+      Kp = 1;
+    }
+    else
+    {
+      Kp = -1;
+    }
+  }
+  if (Kp == 1)
+  {
+    for (StepAlg = 1; StepAlg <=2; ++StepAlg)
+    {
+      z = half(xk, Kp, StepAlg, parameters);
+      if (StepAlg == 1) z_k = z;
+      else z_0 = z;
+    }
+    if (z_k<z_0)
+    {
+      xk = mod(xk+z_k, 2*Pi);
+      StepAlg = 3;
+      z_k = half(xk, Kp, StepAlg, parameters);
+      xk = mod(xk+z_k, 2*Pi);
+    }
+    else
+    {
+      Kp = -1;
+      xk = mod(xk+z_0, 2*Pi);
+      StepAlg = 3;
+      z_k = half(xk, Kp, StepAlg, parameters);
+      xk = mod(xk+z_k, 2*Pi);
+    }
+  }
+  else
+  {
+    for (StepAlg = 1; StepAlg <=2; ++StepAlg)
+    {
+      z = half(xk, Kp, StepAlg, parameters);
+      if (StepAlg == 1) z_k = z;
+      else z_0 = z;
+    }
+    if (z_k < z_0)
+    {
+      Kp = -1;
+      xk = mod(xk+z_k, 2*Pi);
+      StepAlg = 3;
+      z_k = half(xk, Kp, StepAlg, parameters);
+      xk = mod(xk+z_k, 2*Pi);
+    }
+    else
+    {
+      Kp = +1;
+      xk = mod(xk+z_0, 2*Pi);
+      StepAlg = 3;
+      z_k = half(xk, Kp, StepAlg, parameters);
+      xk = mod(xk+z_k, 2*Pi);
+    }
+  }
+
+  RHS[0] = xk;
+  return true;
+}
+
+extern "C"
+{
+  void connectSystem ()
+  {
+    MapProxy::systemFunction = invertor;
+  }
+
+}
+
+
+
---
+++ Simulation/Models/99_Yunus/Original/Pointsinchain/3/1/work.plt
@@ -0,0 +1,78 @@
+reset
+
+set macros
+ArrowStyle="head size 0.015,15 lt 1 lw 2 lc rgb 'gray30' front"
+DataStyleS  = "p ps 0.5 pt 7 lc rgb 'red'"
+DataStyleU  = "d lc rgb 'blue'"
+DataStyleCh = "d lc rgb 'forest-green'"
+
+BoxStyleW = "nohead lt 1 lw 3 lc rgb 'white' front"  
+BoxStyle = "nohead lt 1 lw 1 lc rgb 'blue' front"  
+
+ shift(x)=x>1.5*pi? x - 2*pi : x
+
+unset key
+
+set samples 1000
+set terminal postscript landscape enhanced color blacktext \
+   dashed dashlength 1.0 linewidth 1.0 defaultplex \
+   palfuncparam 2000,0.003 \
+   butt "Helvetica" 20
+
+set output "bif_fm.eps"
+
+L=-pi/2.
+R=3./2.*pi
+D=L
+U=R
+
+set size square 
+
+set xrange [L:R]
+set yrange [D:U]
+
+set border lw 1
+
+set xtics ("-{/Symbol P}/2" L, "0" 0, "3/2*{/Symbol P}" R) scale 0 nomirror offset -0.5, 0
+set ytics ("" D, "0" 0, "3/2*{/Symbol P}" U) scale 0.5 nomirror rotate by 90
+
+set xlabel "x" offset 8, 1.8 
+set ylabel "f(x)" offset 4.2, 3.5 rotate by 90
+
+set arrow from 0, D to 0, U lt 1 lw 2 dt 3 lc rgb "gray30" nohead
+set arrow from L, 0 to R, 0 lt 1 lw 2 dt 3 lc rgb "gray30" nohead
+
+Disc=0.32
+A=Disc
+set arrow from A,D to A,U lt 1 lw 1 dt 3 lc rgb "gray30" nohead front
+set label "" at A,D point pt 7 ps 1.3 lc rgb "black" front
+set label "" at A,D point pt 7 ps 1.0 lc rgb "yellow" front
+set label "d0" at A,D offset 0, 0.8
+
+
+Disc=1.17
+A=Disc
+set arrow from A,D to A,U lt 1 lw 1 dt 3 lc rgb "gray30" nohead front
+set label "" at A,D point pt 7 ps 1.3 lc rgb "black" front
+set label "" at A,D point pt 7 ps 1.0 lc rgb "yellow" front
+set label "d1" at A,D offset 0, 0.8
+
+
+Disc=3.46
+A=Disc
+set arrow from A,D to A,U lt 1 lw 1 dt 3 lc rgb "gray30" nohead front
+set label "" at A,D point pt 7 ps 1.3 lc rgb "black" front
+set label "" at A,D point pt 7 ps 1.0 lc rgb "yellow" front
+set label "d2" at A,D offset 0, 0.8
+
+Disc=4.32
+A=Disc
+set arrow from A,D to A,U lt 1 lw 1 dt 3 lc rgb "gray30" nohead front
+set label "" at A,D point pt 7 ps 1.3 lc rgb "black" front
+set label "" at A,D point pt 7 ps 1.0 lc rgb "yellow" front
+set label "d3" at A,D offset 0, 0.8
+
+plot "cobweb.tna" u (shift($2)):(shift($3)) w l lt 1 lw 1 lc rgb "forest-green",\
+ "f.dat" u (shift($1)):(shift($2)) w p pt 7 ps 0.2 lc rgb "red",\
+x w l lt 1 lw 1 lc rgb "gray20"
+
