adba5cdff5eb49505884ba207acfaa752dc7fdf3
Fabian Weik <fabian.weik@protonmail.com>
2023-01-18 17:22:59 +0100

    automatically create gifs

--- Simulation/execution/execute.py
+++ Simulation/execution/execute.py
@@ -3,6 +3,7 @@
 import shutil
 from pathlib import Path
 from copy import copy
+from typing import List
 
 from util.exceptions import CustomException
 from util.execution import execute_and_wait
@@ -34,8 +35,37 @@ def generate_diagram(diagram: Diagram):
         for frame in frames:
             frame.run()
         
+        synthesize_gif(diagram, frames)
         
-        
+        show_pic(get_final_result_gif_path(diagram))
+
+def synthesize_gif(diagram: Diagram, frames: List[Frame]):
+    args = [
+        'convert',
+        '-delay', '100',
+        '-loop', '0',
+    ]
+
+    for frame in frames:
+        if diagram.options.simple_figure:
+            path = get_simple_result_png_path(frame)
+        else:
+            path = get_result_png_path(frame)
+        args.append(str(path))
+    
+    args.append(str(get_final_result_gif_path(diagram)))
+    
+    execute_and_wait(args)
+    
+    '''
+    execute_and_wait([
+        'gifsicle',
+        '-i', str(get_final_result_gif_path(diagram)),
+        '-O3',
+        '--colors', '16',
+        '-o', str(get_final_result_gif_path(diagram)),
+    ])
+    '''
 
 def show_pic(pic_path: Path):
     execute_and_wait(['imv', str(pic_path)])
@@ -44,3 +74,6 @@ def show_pic(pic_path: Path):
 
 def get_final_result_png_path(diagram: Diagram) -> Path:
     return diagram.path / 'result.png'
+
+def get_final_result_gif_path(diagram: Diagram) -> Path:
+    return diagram.path / 'result.gif'
