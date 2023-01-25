a1f2bee174d985a26816b620dcde96675badda34
Fabian Weik <fabian.weik@protonmail.com>
2023-01-25 15:16:45 +0100

    add illustration for period 10

---
+++ Simulation/Models/99_Yunus/ParameterEffects/E0_hi_P12/make_illustration.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+gnuplot illustration.plt
+
+fragmaster
+pdfcrop illustration.pdf illustration.pdf
+convert -rotate 90 -density 600 -alpha off illustration.pdf illustration.png
+
+imv illustration.png
