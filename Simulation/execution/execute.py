d9c63d20883ccb8186f4ebc047dcac9df6ea99e3
Fabian Weik <fabian.weik@protonmail.com>
2023-03-23 16:56:34 +0100

    add points and cobwebs, also run symbolic analysis for cobwebs

--- Simulation/execution/execute.py
+++ Simulation/execution/execute.py
@@ -8,21 +8,31 @@ from typing import List
 from util.exceptions import CustomException
 from util.execution import execute_and_wait
 from util.ranges import generate_parameters_for_animation
-from configuration.diagrams import Diagram, DiagramType, invert_parameter_ranges, invert_scan
+from configuration.diagrams import (
+    Diagram,
+    DiagramType,
+    invert_parameter_ranges,
+    invert_scan,
+)
 from execution.frame import Frame
 from execution.plotting import get_result_png_path, get_simple_result_png_path
 from execution.composite_plotting import generate_composite_picture
 from execution import composite_plotting
 
+
 def generate_diagram(diagram: Diagram):
     if diagram.animation:
         generate_animated_diagram(diagram)
-    
+
     elif diagram.type == DiagramType.PERIOD_REGIONS:
         generate_regions_diagram(diagram)
-    
+
     else:
         generate_simple_diagram(diagram)
+
+    if diagram.type == DiagramType.COBWEB:
+        process_symbolic(diagram)
+
 
 def generate_simple_diagram(diagram: Diagram):
     frame = Frame(diagram, 0)
@@ -30,7 +40,7 @@ def generate_simple_diagram(diagram: Diagram):
 
     if diagram.type == DiagramType.ANALYSIS:
         return
-    
+
     if diagram.options.simple_figure:
         final_result_png_path = get_final_simple_result_png_path(diagram)
         result_png_path = get_simple_result_png_path(frame)
@@ -41,7 +51,8 @@ def generate_simple_diagram(diagram: Diagram):
     shutil.copy(result_png_path, final_result_png_path)
     if not diagram.options.dont_show:
         show_pic(result_png_path)
-    
+
+
 def generate_regions_diagram(diagram: Diagram):
     frames = [
         Frame(diagram, 0),
@@ -49,10 +60,10 @@ def generate_regions_diagram(diagram: Diagram):
         Frame(diagram, 2, scan=invert_scan(diagram.scan)),
         Frame(diagram, 3, scan=invert_scan(invert_parameter_ranges(diagram.scan, 1))),
     ]
-    
+
     for frame in frames:
         frame.run()
-    
+
     generate_composite_picture(frames)
 
     if diagram.options.simple_figure:
@@ -66,29 +77,33 @@ def generate_regions_diagram(diagram: Diagram):
     if not diagram.options.dont_show:
         show_pic(result_png_path)
 
+
 def generate_animated_diagram(diagram: Diagram):
     cnt = 0
     frames = []
     for parameters in generate_parameters_for_animation(diagram.animation):
         frames.append(Frame(diagram, cnt, parameters))
         cnt += 1
-    
+
     for frame in frames:
         frame.run()
-    
+
     if diagram.type == DiagramType.ANALYSIS:
         return
-    
+
     synthesize_gif(diagram, frames)
-    
+
     if not diagram.options.dont_show:
         show_pic(get_final_result_gif_path(diagram))
 
+
 def synthesize_gif(diagram: Diagram, frames: List[Frame]):
     args = [
-        'convert',
-        '-delay', str(50),
-        '-loop', '0',
+        "convert",
+        "-delay",
+        str(50),
+        "-loop",
+        "0",
     ]
 
     for frame in frames:
@@ -97,12 +112,12 @@ def synthesize_gif(diagram: Diagram, frames: List[Frame]):
         else:
             path = get_result_png_path(frame)
         args.append(str(path))
-    
+
     args.append(str(get_final_result_gif_path(diagram)))
-    
+
     execute_and_wait(args)
-    
-    '''
+
+    """
     execute_and_wait([
         'gifsicle',
         '-i', str(get_final_result_gif_path(diagram)),
@@ -110,18 +125,30 @@ def synthesize_gif(diagram: Diagram, frames: List[Frame]):
         '--colors', '16',
         '-o', str(get_final_result_gif_path(diagram)),
     ])
-    '''
+    """
+
 
 def show_pic(pic_path: Path):
-    execute_and_wait(['imv', str(pic_path)])
+    execute_and_wait(["imv", str(pic_path)])
+
+
+# Symbolic Sequences
+
+
+def process_symbolic(diagram: Diagram):
+    pass
+
 
 # Path utils
 
+
 def get_final_result_png_path(diagram: Diagram) -> Path:
-    return diagram.path / 'result.png'
+    return diagram.path / "result.png"
+
 
 def get_final_simple_result_png_path(diagram: Diagram) -> Path:
-    return diagram.path / 'result-simple.png'
+    return diagram.path / "result-simple.png"
+
 
 def get_final_result_gif_path(diagram: Diagram) -> Path:
-    return diagram.path / 'result.gif'
+    return diagram.path / "result.gif"
