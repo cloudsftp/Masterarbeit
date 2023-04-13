1d9484815cde74d554c23e6fae1076f5238c61f0
Fabian Weik <fabian.weik@protonmail.com>
2023-04-13 12:14:29 +0200

    multicolored cobweb test

---
+++ Simulation/Models/60_MinimalRepr/Cobweb_Y/Manual/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+AnT "../../model.so" -i "configB.ant"
+AnT "../../model.so" -i "configA1.ant"
+AnT "../../model.so" -i "configA2.ant"
+
+gnuplot "plot.plt"
+
+fragmaster
+pdfcrop "result.pdf" "result.pdf"
+convert -rotate 90 -density 600 -alpha off "result.pdf" "result.png"
+
+imv "result.png"
