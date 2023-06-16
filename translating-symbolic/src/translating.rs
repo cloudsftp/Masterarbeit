3d53b159e10b60be77226b39625224bcd1e8b5cf
Fabian Weik <fabian.weik@protonmail.com>
2023-06-16 11:13:34 +0200

    convert simplest case halved -> full

--- translating-symbolic/src/translating.rs
+++ translating-symbolic/src/translating.rs
@@ -1,9 +1,32 @@
+use std::borrow::BorrowMut;
+
 use crate::cycles::{FullCycle, HalvedCycle, Sequence};
 
 impl From<HalvedCycle> for FullCycle {
     fn from(value: HalvedCycle) -> Self {
-        todo!()
+        let og_len = value.sequence.len();
+        let mut seq = value.sequence.clone();
+        seq.extend_from_within(..);
+        seq.extend_from_within(..seq.len() / 2);
+
+        let new_seq = translate_to_full_from_index(&seq, 0, og_len);
+        FullCycle { sequence: new_seq }
     }
+}
+
+fn translate_to_full_from_index(seq: &Sequence, start: usize, og_len: usize) -> Sequence {
+    let mut seq = seq[start..start + 2 * og_len].to_vec();
+
+    for i in 0..seq.len() {
+        let c = (i - start) % 4;
+        if c < 2 {
+            assert_eq!(seq[i].0, c);
+        } else {
+            seq[i].0 = c;
+        }
+    }
+
+    smallest_cycle(seq)
 }
 
 impl From<FullCycle> for HalvedCycle {
@@ -56,5 +79,20 @@ mod test {
         };
         let halved: HalvedCycle = full.into();
         assert_eq!(halved.sequence, vec![(0, 1), (1, 2)]);
+    }
+
+    #[test]
+    fn to_full() {
+        let halved = HalvedCycle {
+            sequence: vec![(0, 1), (1, 2)],
+        };
+        let full: FullCycle = halved.into();
+        assert_eq!(full.sequence, vec![(0, 1), (1, 2), (2, 1), (3, 2)]);
+
+        let halved = HalvedCycle {
+            sequence: vec![(0, 1), (1, 2), (0, 5), (1, 7)],
+        };
+        let full: FullCycle = halved.into();
+        assert_eq!(full.sequence, vec![(0, 1), (1, 2), (2, 5), (3, 7)]);
     }
 }
