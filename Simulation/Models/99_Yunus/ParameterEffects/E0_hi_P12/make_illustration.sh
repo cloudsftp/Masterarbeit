0b0346f795f9c65db64a0aadb581f9a7aa1a8764
Fabian Weik <fabian.weik@protonmail.com>
2023-08-19 17:24:32 +0200

    almost finished with corrections of setup

--- Simulation/Models/99_Yunus/ParameterEffects/E0_hi_P12/make_illustration.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gnuplot illustration.plt
-
-fragmaster
-pdfcrop illustration.pdf illustration.pdf
-convert -rotate 90 -density 600 -alpha off illustration.pdf illustration.png
-
-imv illustration.png
