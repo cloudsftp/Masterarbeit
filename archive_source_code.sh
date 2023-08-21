42363afb887a2a66ac8116b8abb1d8d49ad846c4
Fabian Weik <fabian.weik@protonmail.com>
2023-08-21 21:29:54 +0200

    add archiving step to workflow

--- archive_source_code.sh
+++ archive_source_code.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+FOLDERS=(symbolic-regions translating-symbolic Report Figures Simulation)
+
 TARGET_DIR="Masterarbeit_Weik_SourceCode"
+TARGET_ARCHIVE_PICS="${TARGET_DIR}_WithPictures.tar.gz"
 TARGET_ARCHIVE="${TARGET_DIR}.tar.gz"
 
-cp -r symbolic-regions "${TARGET_DIR}"
-cp -r translating-symbolic "${TARGET_DIR}"
+mkdir -p "${TARGET_DIR}"
+for folder in "${FOLDERS[@]}"; do
+    cp -r "${folder}" "${TARGET_DIR}"
+done
 
-cp -r Simulation "${TARGET_DIR}"
-cd "${TARGET_DIR}/Simulation"
-find . -name '*.png'
+tar cfz "${TARGET_ARCHIVE_PICS}" "${TARGET_DIR}"
+du -h "${TARGET_ARCHIVE_PICS}"
+
+cd "${TARGET_DIR}"
+find . -name '*.png' | xargs -I{} rm {}
+cd - &> /dev/null
 
 tar cfz "${TARGET_ARCHIVE}" "${TARGET_DIR}"
 du -h "${TARGET_ARCHIVE}"
+
+rm -rf "${TARGET_DIR}"
