0b790d4cedce42bb9f475e218e8a0be257f99add
Fabian Weik <fabian.weik@protonmail.com>
2023-08-22 07:47:28 +0200

    finish figures for 6 dynamics

---
+++ Figures/6/6.5a/model.cpp
@@ -0,0 +1,76 @@
+#include "AnT.hpp"
+
+// External parameters
+#define aL parameters[0]
+#define bL parameters[1]
+#define cL parameters[2]
+#define px parameters[3] // Only the last two are varied
+#define py parameters[4]
+
+// Internal parameters for computing branches A and C
+#define _aL (aL)
+#define _bL (bL)
+#define _cL (cL + py)
+
+// Internal parameters for fitting branches B and D
+#define A (-px)
+#define B (0.525)
+
+// Internal parameters for computing branches B and D
+#define _bR (4. * (B - A))
+#define _cR ((2. * A) - B)
+
+#define n       0.5     // Discontinuity in the middle
+#define border  0.25    // Discontinuity in the middle of the left half
+
+bool f(
+    const Array<real_t>& currentState,
+    const Array<real_t>& parameters,
+    Array<real_t>& RHS
+) {
+    real_t y = 0;
+
+    real_t x = currentState[0];
+    // Enforce symmetry f(x + n) = f(x) + n
+    if (x >= n) {
+        x -= n;
+        y += n;
+    }
+
+    if (x < border) {   // "Left" branch (branches A and C)
+        y += _aL * x * x + _bL * x + _cL;
+    } else {            // "Right" branch (branches B and D)
+        y += _bR * x + _cR;
+    }
+
+    // Normalize
+    if (y >= n) {
+        y -= n;
+    }
+
+    RHS[0] = y;
+    return true;
+}
+
+bool symbolic(
+    const Array<real_t>& currentState,
+    const Array<real_t>& parameters,
+    string& RHS
+) {
+    real_t x = currentState[0];
+
+    if (x < border) {
+        RHS = "A";
+    } else {
+        RHS = "B";
+    }
+
+    return true;
+}
+
+extern "C" {
+    void connectSystem() {
+        MapProxy::systemFunction = f;
+        MapProxy::symbolicFunction = symbolic;
+    }
+}
---
+++ Simulation/Models/61_MinimalRepr_Halved/2D_Regions_F/extras.plt
@@ -0,0 +1,12 @@
+
+set label 6 "Q" at -0.375, 0.1678 point pointtype 7 lc rgb 'red' front
+
+set label 7 "Rp" at -0.375, 0.16853 point pointtype 7 lc rgb 'red' front
+set label 8 "S" at -0.371, 0.1676 point pointtype 7 lc rgb 'red' front
+set label 9 "T" at -0.377, 0.1669 point pointtype 7 lc rgb 'red' front
+set label 10 "Up" at -0.3797, 0.168 point pointtype 7 lc rgb 'red' front
+
+set label 11 "V" at -0.3695, 0.1683 point pointtype 7 lc rgb 'red' front
+set label 12 "W" at -0.3735, 0.1665 point pointtype 7 lc rgb 'red' front
+set label 13 "X" at -0.3805, 0.1672 point pointtype 7 lc rgb 'red' front
+set label 14 "Y" at -0.379, 0.16865 point pointtype 7 lc rgb 'red' front
